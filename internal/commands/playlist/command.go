5de2895f6ecee2bc1db6cb975f3b0a385f77f934
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2022-10-29 12:47:24 +0200

    Fixed download streams

--- internal/commands/playlist/command.go
+++ internal/commands/playlist/command.go
@@ -3,12 +3,14 @@ package playlist
 import (
 	"RiffSpot/internal/providers/spotify"
 	"RiffSpot/internal/utils/downloadutils"
+	"fmt"
 	"path/filepath"
 )
 
 func Execute(playlistId string, outputFolder string) {
 	for _, spotifyTrack := range spotify.GetTracks(playlistId) {
 		videoId := downloadutils.FindClosestMatch(&spotifyTrack).VideoID
+		fmt.Println()
 		downloadutils.DownloadSong(videoId, filepath.Join(outputFolder, spotifyTrack.Track.Name+".mp3"))
 	}
 }
--- internal/utils/downloadutils/utils.go
+++ internal/utils/downloadutils/utils.go
@@ -3,70 +3,88 @@ package downloadutils
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
+	"math"
+
 	"github.com/raitonoberu/ytmusic"
 	"github.com/schollz/progressbar/v3"
 	ffmpeg_go "github.com/u2takey/ffmpeg-go"
 	"github.com/wader/goutubedl"
 	"github.com/zmb3/spotify/v2"
-	"io"
-	"log"
-	"math"
 )
 
 func FindClosestMatch(spotifyTrack *spotify.PlaylistItemTrack) *ytmusic.TrackItem {
-	s := ytmusic.Search(spotifyTrack.Track.Name)
-	result, err := s.Next()
+	searchString := fmt.Sprintf(
+		"%s - %s",
+		spotifyTrack.Track.Name,
+		spotifyTrack.Track.Artists[0].Name,
+	)
 
+	search := ytmusic.Search(searchString)
+	result, err := search.Next()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	minDiff := math.Abs(float64(spotifyTrack.Track.Duration - result.Tracks[0].Duration))
 	closestTrack := result.Tracks[0]
 
-	for _, i := range result.Tracks {
-		diff := math.Abs(float64(spotifyTrack.Track.Duration - i.Duration))
+	for _, track := range result.Tracks {
+		diff := math.Abs(float64(spotifyTrack.Track.Duration - track.Duration))
 
 		if diff < minDiff {
 			minDiff = diff
-			closestTrack = i
+			closestTrack = track
 		}
 	}
 
 	return closestTrack
 }
 
-func DownloadSong(youtubeId string, path string) {
+func DownloadSong(youtubeId string, outputPath string) {
 	log.SetOutput(io.Discard)
 	goutubedl.Path = "yt-dlp"
 
+	// Create new youtube download
 	result, err := goutubedl.New(
 		context.Background(),
 		youtubeId,
 		goutubedl.Options{Type: goutubedl.TypeSingle},
 	)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	dr, err := result.Download(context.Background(), "best")
+	// Start downloading the video
+	downloadResult, err := result.Download(context.Background(), "best")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bar := progressbar.DefaultBytes(
+	// Create a progress bar
+	progressBar := progressbar.DefaultBytes(
 		int64(result.Info.FilesizeApprox),
 		"Downloading "+result.Info.Title,
 	)
+	defer progressBar.Close()
 
-	com := ffmpeg_go.Input("pipe:").Audio().Output(path).Compile()
-	com.Stdin = dr
+	// Create ffmpeg audio extractor
+	ffmpegCmd := ffmpeg_go.Input("pipe:").Audio().Output(outputPath).Compile()
+
+	// Retrieve the standard input pipe of ffmpeg
+	ffmpegStdin, err := ffmpegCmd.StdinPipe()
 	if err != nil {
-		fmt.Println("Error transcoding")
+		log.Fatal(err)
 	}
-	io.Copy(bar, dr)
 
-	err = com.Run()
-	bar.Close()
+	// Run the ffmpeg command in background
+	err = ffmpegCmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Pipe youtube download stream to progress bar and ffmpeg
+	io.Copy(io.MultiWriter(progressBar, ffmpegStdin), downloadResult)
+
 }
